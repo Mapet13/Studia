cad33229c890e1e154a1d86e6c26e75206abe7de
Mapet <jakubsordyl1@gmail.com>
2020-10-12 14:39:22 +0200

    add some comments to task 8

--- 8.py
+++ 8.py
@@ -6,15 +6,15 @@ skoro mamy nie uzywac list to standardowy algorytm a nie sito Eratostenesa
 
 target = int(input("Podaj liczbę: "))
 
-is_prime = True
+is_prime = True          # wartośc domyslnie ustawiona na True, jesli sie okaze ze jednak target to nie liczba pierwsza to wartosc tej zmiennej ulegnie zmianie
 
 if target > 1:
     i = 2
-    while (i*i <= target):
-        if target % i == 0:
+    while (i*i <= target):      # wystarczy sprawdzac liczby do sqrt(target)
+        if target % i == 0:     # jezeli znajdzie się dzielnik 
             is_prime = False
             break
-        i += 10
+        i += 1                  
 
 if is_prime:
     print(f"Tak, {target} jest liczba pierwszą")
