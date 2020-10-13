b799c9a5a273e5be1ff06d21b31b425eabb37206
Mapet <jakubsordyl1@gmail.com>
2020-10-13 15:39:25 +0200

    fix task 2

--- 02.py
+++ 02.py
@@ -5,16 +5,16 @@ aby w ciągu analogicznym do ciągu Fibonacciego wystąpił wyraz równy numerow
 troche roziązanie na pałe ale nie widze lepszego aktualnie
 """
 
-target = 2020
+target = 13844
 
 a = int(target / 2)
 b = target - a
 best = (a, b) # w taki sposob aby napewno byla tu suma zarowno dla nieparzystych jak i parzystych
 
 for i in range(1, a):
-    for j in range(1, b):   # przeszukuje kolejne pary mniejsze od a i b 
-        x = i               # zapisuje pierwsze elementy do x i y 
-        y = j               
+    for j in range(1, i+1):   # przeszukuje kolejne pary mniejsze od a i b (i, j) gdzie j jest mniejszy niz i 
+        x = j                 # zapisuje pierwsze elementy do x i y (x - mniejsza)(y - wieksza)
+        y = i               
         sum = x + y
         while sum < target: # dalej standardowo jak w ciagu Fibonacciego
             sum = x + y
@@ -22,9 +22,7 @@ for i in range(1, a):
             y = sum
             if sum == target:                   
                 if i + j < best[0] + best[1]:   # sprawdzam czy aktualna suma poczatkowych wyrazow jest mniejsza niz obecnie najlepsza
-                    best = (i, j)
+                    best = (j, i)
 
 
 print(best)
-
-                
