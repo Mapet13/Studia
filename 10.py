ff119c8e378c9e6bc8c7b34ba27957c6f94d4c66
Mapet <jakubsordyl1@gmail.com>
2020-10-13 14:29:07 +0200

    modify tasks 10 and 11 for better performance

--- 10.py
+++ 10.py
@@ -1,11 +1,22 @@
 """
 Napisać program wyszukujący liczby doskonałe mniejsze od miliona.
+
+liczby doskonałe to takie:
+    ze suma równają się swojej sumie dzielnikow
 """
+from math import sqrt
 
 for i in range(2, int(10e6)):            
     sum = 0
-    for divisor in range(i-1, 0, -1):      # iteruje od i-1 w dół sprawdzajc kolejne liczby czy nie są dzielnikami i
-        if i % divisor == 0:        
-            sum += divisor        
+    for divisor in range(1, int(sqrt(i)) + 1):                  # iteruje od 1 do sqrt(i) + 1
+        if i % divisor == 0:                                    # spr podzielnosc
+            sum += divisor                                      # dodaje dzielnik
+            if i // divisor != divisor and i // divisor != i:     # ==> czyli jesli nie dziele przez 1 lub nie dziele przez sqrt(x)
+                sum += i // divisor                              #   to wtedy suma dodatkowo jest rowna temu (X/N)
     if (sum == i):
         print(i)
+        
+"""
+    jest to robione bo wiadomo ze jesli X dzieli sie liczba przez N to bedzie sie tez dzielic przez X/N
+    a wiadomo ze nie przerobimy 2 razy X/N bo liczymy do pierwiastka
+"""
