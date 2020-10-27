09ace6b693364090019488f832ab1ba127eb1171
Mapet <jakubsordyl1@gmail.com>
2020-10-27 10:15:39 +0100

    add tasks from 3rd set

---
+++ ćwiczenia 3/03.py
@@ -0,0 +1,17 @@
+'''
+Napisać program generujący i wypisujący liczby pierwsze mniejsze od N metodą Sita Eratostenesa.
+'''
+
+n = int(input())
+
+tab = [True] * n
+
+i = 2
+while i < n:
+    if tab[i]:
+        print(i)
+        j = 2 * i
+        while j < n:
+            tab[j] = False
+            j += i
+    i += 1
