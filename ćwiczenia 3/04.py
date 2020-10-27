380e7ebf0f222d5b50e8f2aaf0abb58f805a4273
Mapet <jakubsordyl1@gmail.com>
2020-10-27 12:13:05 +0100

    add task description to task 04

--- ćwiczenia 3/04.py
+++ ćwiczenia 3/04.py
@@ -1,3 +1,9 @@
+'''
+Napisać program obliczający i wypisujący stałą e z rozwinięcia w szereg 
+e = 1/0! + 1/1! + 1/2! + 1/3! + ... 
+z dokładnością N cyfr dziesiętnych (N jest rzędu 1000).
+'''
+
 n = int(input("n: "))
 
 result = 0
