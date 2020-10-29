a09ceb6e18cb82304231a5c1611664e0938e023e
Mapet13 <jakubsordyl1@gmail.com>
2020-10-29 13:25:49 +0100

    Update 03.py

--- ćwiczenia 3/03.py
+++ ćwiczenia 3/03.py
@@ -1,17 +1,18 @@
 '''
 Napisać program generujący i wypisujący liczby pierwsze mniejsze od N metodą Sita Eratostenesa.
 '''
+from math import sqrt
 
-n = int(input())
+n = int(input("n = "))
 
-tab = [True] * n
+t = [True] * n
+t[0] = t[1] = False
 
-i = 2
-while i*i <= n:
-    if tab[i]:
+for i in range(2, int(sqrt(n))):
+    if t[i]:
+        for j in range(2 * i, n, i):
+            t[j] = False
+
+for i in range(1, n):
+    if t[i]:
         print(i)
-        j = 2 * i
-        while j < n:
-            tab[j] = False
-            j += i
-    i += 1
