f448f1864a4cff9e671775327bed4c5de13d348e
Mapet <jakubsordyl1@gmail.com>
2020-11-09 10:30:42 +0100

    change to proper task description

--- ćwiczenia 4/07.py
+++ ćwiczenia 4/07.py
@@ -1,10 +1,8 @@
 '''
-Dane są dwie tablice mogące pomieścić taką samą liczbę elementów: 
-T1[N][N] i T2[M], gdzie M=N*N. 
-W każdym wierszu tablicy T1 znajdują się uporządkowane rosnąco (w obrębie wiersza) liczby naturalne. 
-Proszę napisać funkcję przepisującą wszystkie singletony (liczby występujące dokładnie raz) z
-tablicy T1 do T2, tak aby liczby w tablicy T2 były uporządkowane rosnąco. Pozostałe elementy tablicy T2
-powinny zawierać zera.
+Dane są dwie tablice mogące pomieścić taką samą liczbę elementów: T1[N][N] i T2[M], gdzie M=N*N. 
+W każdym wierszu tablicy T1 znajdują się uporządkowane niemalejąco (w obrębie wiersza) liczby naturalne. 
+Proszę napisać funkcję przepisującą wszystkie liczby z tablicy T1 do T2, 
+tak aby liczby w tablicy T2 były uporządkowane niemalejąco.
 '''
 
 def f(t1, t2):
