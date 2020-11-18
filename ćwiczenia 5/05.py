671d16753be7c2f2f61bf81e63e2a17a854847e0
Mapet <jakubsordyl1@gmail.com>
2020-11-18 14:43:21 +0100

    fix task 05

--- ćwiczenia 5/05.py
+++ ćwiczenia 5/05.py
@@ -7,22 +7,13 @@ Proszę napisać funkcję, która zwraca  wartość True jeżeli zbiorze istniej
 Do funkcji należy przekazać strukturę opisującą położenie punktów.
 '''
 
+def is_between_points(x, a, b):
+    return (x >= a and x <= b) or (x <=  a and x >= b)
 
-def get_distance_pow(a, b):
-    return ((a[0] - b[0]) ** 2) + ((a[1] - b[1]) ** 2) 
-
-def get_trangle_area(a, b, c):
-    return 0.5 * abs(((b[0] - a[0]) * (c[1] - a[1])) - ((b[1] - a[1]) * (c[0] - a[0])))
-
-def are_point_in_square(p, a, b, c, d, area):
-    area_with_p = get_trangle_area(a, b, p)
-    area_with_p += get_trangle_area(a, c, p)
-    area_with_p += get_trangle_area(d, b, p)
-    area_with_p += get_trangle_area(d, c, p)
-    
-    if area_with_p == area:
-        return True
-    
+def is_point_in_square(p, a, b, c, d):
+    if is_between_points(p[1], a[1], b[1]):
+        if is_between_points(p[0], a[0], c[0]):
+            return True
     return False
         
 
@@ -31,83 +22,84 @@ def does_square_exist(t):
     
     for i in range(n):
         a = t[i] # wubieram 1 pkt
-        for j in range(i + 1, n):
-            if j == i:
-                continue
-            b = t[j] # wybieram 2 pkt
-            side_len = get_distance_pow(a, b) # kwadrat dlugosci bo nie musze dawac sqrt bo po co
-            for k in range(n):
-                if k == i or k == j:
-                    continue
-                c = t[k]
-                if side_len == get_distance_pow(a, c) and (2 * side_len == get_distance_pow(b, c)): # jesl te 3 pkt tworzą trójkat prostokątny o równych przeciwprostokątnych
-                    for g in range(n):
-                        if g == i or g == j or g == k:
-                            continue
-                        d = t[g]
-                        if get_distance_pow(d, c) == get_distance_pow(d, b):
-                            all_points_outside_square = True
-                            for x in range(n):
-                                if x != i and x != j and x != k and x != g:
-                                    p = t[x]
-                                    if are_point_in_square(p, a, b, c, d, side_len): # moge jako pole przesłac side_len bo nie pierwiastkowałem tego wiec to jakby a**a
-                                        all_points_outside_square = False
-                                        break
-                            if all_points_outside_square:
-                                print(a, b, c, d)
-                                return True
+        
+        for j in range(i+1, n): # moge tak bo dla wczesniejszych juz sprawdziłem równoległość z A (na osi X)
+            b = t[j]
+            
+            if b[0] == a[0] and b[1] != a[1]:
+                for k in range(n):
+                    if k != i and k != j:
+                        c = t[k]
+                        if c[0] != a[0]:
+                            c_is_close_to_a = None
+                            if c[1] == a[1]: 
+                                c_is_close_to_a = True
+                            elif c[1] == b[1]: 
+                                c_is_close_to_a = False
+                            else:
+                                continue
+                            for g in range(k+1, n):
+                                d = t[g]
+                                if d[0] == c[0]:
+                                    if (c_is_close_to_a and d[1] == b[1]) or (not c_is_close_to_a and d[1] == a[1]):
+                                        # w tym miejscu juz wiem że mam kwadrat ale musze sprawdzić punkty
+                                        all_points_outside_square = True
+                                        for x in range(n):
+                                            if x != i and x != j and x != k and x != g:
+                                                if is_point_in_square(t[x], a, b, c, d):
+                                                    all_points_outside_square = False
+                                                    break
+                                        if all_points_outside_square:
+                                            print(a, b, c, d)
+                                            return True
+            
     return False
                                         
-print(does_square_exist([ # ((0, 4) (4, 6) (2, 0) (6, 2))
+print(does_square_exist([ # (0, 4) (2, 4) (2, 6) (0, 6)
     (0, 4),
-    (4, 6),
-    (2, 0),
-    (6, 2)
+    (0, 6),
+    (2, 4),
+    (2, 6)
 ]))
 
-print(does_square_exist([ # (4, 6) (6, 2) (0, 4) (2, 0)
-    (4, 6),
-    (6, 2),
-    (2, 0),
-    (0, 4)
+print(does_square_exist([ # False
+    (0, 4),
+    (0, 6),
+    (2, 4),
+    (2, 6),
+    (0, 6)
 ]))
 
-print(does_square_exist([ # (2, 0) (6, 2) (0, 4) (4, 6)
-    (2, 0),
-    (6, 2),
-    (4, 6),
-    (0, 4)
+print(does_square_exist([ # False
+    (0, 4),
+    (0, 6),
+    (2, 4),
+    (2, 6),
+    (1, 6),
 ]))
 
-print(does_square_exist([ # false 
-    (2, 0),
-    (6, 2),
-    (4, 4),
-    (4, 6),
-    (0, 4) 
+print(does_square_exist([ # False
+    (0, 4),
+    (0, 6),
+    (2, 4),
+    (2, 6),
+    (1, 5)
 ]))
 
-print(does_square_exist([ # (2, 0) (6, 2) (0, 4) (4, 6)
-    (-1, 1),
-    (5, 7),
+print(does_square_exist([ #(0, 4) (-1, 4) (-1, 3) (0, 3)
+    (0, 4),
+    (0, 6),
+    (2, 4),
+    (2, 6),
     (1, 5),
-    (7, 3),
-    (3, 1),
-    (2, -1),
-    (3, -5),
-    (7, 1),
+    (0, 3),
+    (-1, 3),
+    (-1, 4)
 ]))
 
-print(does_square_exist([ # false
-    (-1, 1),
-    (5, 7),
-    (1, 5),
-    (7, 3),
-    (3, 1),
-    (5, 5),
-    (2, -1),
-    (3, -5),
-    (7, 1),
+print(does_square_exist([ # (-3, -3) (-1, -3) (-1, -1) (-3, -1)
+    (-1, -1),
+    (-3, -1),
+    (-3, -3),
+    (-1, -3)
 ]))
-
-
