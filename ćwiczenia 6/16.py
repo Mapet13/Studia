2cf39b79b8007d7b34e7621326ee200a9c8670f8
Mapet <jakubsordyl1@gmail.com>
2020-12-06 11:18:15 +0100

    add task description

--- ćwiczenia 6/16.py
+++ ćwiczenia 6/16.py
@@ -1,3 +1,13 @@
+'''
+Wyrazy budowane są z liter a..z. 
+Dwa wyrazy ”ważą” tyle samo jeżeli: mają tę samą liczbę samogłosek 
+oraz sumy kodów ascii liter z których są zbudowane są identyczne, na przykład 
+"ula" → 117, 108, 97 oraz "exe" → 101, 120, 101. 
+Proszę napisać funkcję wyraz(s1,s2), która sprawdza 
+czy jest możliwe zbudowanie wyrazu z podzbioru liter zawartych w s2 ważącego tyle co wyraz s1. 
+Dodatkowo funkcja powinna wypisać znaleziony wyraz.
+'''
+
 vowels = ['A', 'Ą', 'E', 'Ę', 'I', 'O', 'U', 'Y']
 
 def count_vowels(s):
