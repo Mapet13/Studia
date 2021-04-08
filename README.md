c4907319abc57ce50077e0a93aa03995fc2f58bd
Mapet <jakubsordyl1@gmail.com>
2021-04-08 12:23:31 +0200

    change repo style

---
+++ README.md
@@ -0,0 +1,5 @@
+# This repo contains most of my programming tasks from studying computer science at AGH UST
+
+#### Courses:
+
+* [WID - Introduction to Computer Science](https://github.com/Mapet13/Studia/tree/WDI)
--- ćwiczenia 6/Na Zajęciach 3/13.py
+++
@@ -1,8 +0,0 @@
-def podzial(n, k = 1, l=""):
-    if n == 0 and '+' in l[:-1]:
-        print(l[:-1])
-    else:    
-        for i in range(k, n+1):
-            podzial(n-i, i, l + str(i) + "+")
-          
-podzial(10)
