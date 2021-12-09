9728233e0de49875f58b4a16e12888632fc1fef5
Mapet <jakubsordyl1@gmail.com>
2021-12-09 11:03:37 +0100

    add some tasks

---
+++ lab4/ex2.hs
@@ -0,0 +1,16 @@
+-- product type example (one constructor)
+data CartInt2DVec = MkCartInt2DVec Int Int -- konwencja: prefix 'Mk' dla konstruktora
+
+xCoord :: CartInt2DVec -> Int
+xCoord (MkCartInt2DVec x _) = x
+
+yCoord :: CartInt2DVec -> Int
+yCoord (MkCartInt2DVec _ y) = y
+
+data Cart2DVec' a = MkCart2DVec' a a
+
+xCoord' :: Cart2DVec' a -> a
+xCoord' (MkCart2DVec' x _) = x
+
+yCoord' :: Cart2DVec' a -> a
+yCoord' (MkCart2DVec' _ y) = y
