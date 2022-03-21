bf0d4f1a862811b7aa429176ba86c0a30db786eb
Mapet13 <jakubsordyl1@gmail.com>
2022-03-21 22:42:20 +0100

    update lab2/zad3

--- lab_02/zad3/fwalk.c
+++ lab_02/zad3/fwalk.c
@@ -6,6 +6,7 @@
 #include <dirent.h>
 #include <unistd.h> 
 #include <sys/stat.h>
+#include <ftw.h>
 
 /////////////////////////////////////////////////////////////////////////
 #define CHECK __log__("CHECK", __TIMESTAMP__, __FILE__, __LINE__)
@@ -16,6 +17,8 @@ void __log__(char* mess, char* time, char* file, int line) {
 }
 /////////////////////////////////////////////////////////////////////////
 
+
+
 typedef struct {
     size_t reg;
     size_t chr;
@@ -24,7 +27,9 @@ typedef struct {
     size_t sock;
 } Counter; 
 
-void print_info(const char* dir_path, Counter* counter);
+Counter* g_counter;
+
+void print_info(const char* dir_path);
 
 int main(int argc, char** argv) {
     if (argc != 2)
@@ -32,19 +37,19 @@ int main(int argc, char** argv) {
     
     char* root_path = argv[1];  
 
-    Counter* counter = calloc(sizeof(*counter), 1);
+    g_counter = calloc(sizeof(*g_counter), 1);
 
-    print_info(root_path, counter);
+    print_info(root_path);
 
     puts("**********************");
-    printf("reg: %ld \n", counter->reg);
-    printf("chr: %ld \n", counter->chr);
-    printf("dir: %ld \n", counter->dir);
-    printf("fifo: %ld \n", counter->fifo);
-    printf("sock: %ld \n", counter->sock);
+    printf("reg: %ld \n", g_counter->reg);
+    printf("chr: %ld \n", g_counter->chr);
+    printf("dir: %ld \n", g_counter->dir);
+    printf("fifo: %ld \n", g_counter->fifo);
+    printf("sock: %ld \n", g_counter->sock);
     puts("**********************");
 
-    free(counter);
+    free(g_counter);
 }
 
 char* get_file_path(const char* dir_path, const char* file_name) {
@@ -59,7 +64,34 @@ int is_special(const char* file_name) {
     return strcmp(file_name, ".") == 0 || strcmp(file_name, "..") == 0;
 }
 
-void print_info(const char* dir_path, Counter* counter) {
+#ifdef NFTW_MODE
+int action(const char* file_path, const struct stat* stat_buf, int fileflags, struct FTW* pfwt) {
+    switch(stat_buf->st_mode & S_IFMT) {
+        case S_IFREG: 
+            printf("%s: %ld, %d, %ld, %ld, %ld \n", file_path, stat_buf->st_nlink, stat_buf->st_mode, stat_buf->st_size, stat_buf->st_atime, stat_buf->st_mtime);
+            g_counter->reg++;
+            break;
+        case S_IFCHR: 
+            g_counter->chr++;
+            break;
+        case S_IFDIR: 
+            g_counter->dir++;
+            break;
+        case S_IFIFO: 
+            g_counter->fifo++;
+            break;
+        case S_IFSOCK: 
+            g_counter->sock++;
+            break;
+    }
+    return 0;
+}
+
+void print_info(const char* dir_path) {
+    nftw(dir_path, &action, 100, 0);
+}
+#else
+void print_info(const char* dir_path) {
     DIR* dir = opendir(dir_path);    
     
     if(!dir) 
@@ -76,22 +108,22 @@ void print_info(const char* dir_path, Counter* counter) {
         switch(stat_buf.st_mode & S_IFMT) {
             case S_IFREG: 
                 printf("%s: %ld, %d, %ld, %ld, %ld \n", file_path, stat_buf.st_nlink, stat_buf.st_mode, stat_buf.st_size, stat_buf.st_atime, stat_buf.st_mtime);
-                (counter->reg)++;
+                g_counter->reg++;
                 break;
             case S_IFCHR: 
-                (counter->chr)++;
+                g_counter->chr++;
                 break;
             case S_IFDIR: 
                 if(!is_special(obj->d_name)) {
-                    (counter->dir)++;
-                    print_info(file_path, counter);
+                    g_counter->dir++;
+                    print_info(file_path);
                 }
                 break;
             case S_IFIFO: 
-                (counter->fifo)++;
+                g_counter->fifo++;
                 break;
             case S_IFSOCK: 
-                (counter->sock)++;
+                g_counter->sock++;
                 break;
         }
 
@@ -100,3 +132,4 @@ void print_info(const char* dir_path, Counter* counter) {
 
     closedir(dir);
 }
+#endif
