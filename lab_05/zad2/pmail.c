aae2577388d86d78545873e3074a62bb79587d97
Mapet13 <jakubsordyl1@gmail.com>
2022-04-10 14:15:26 +0200

    add lab 5: task 02

--- lab_05/zad2/pmail.c
+++ lab_05/zad2/pmail.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 256
+
+const char* commandSortMode(const char* str) {
+	if(strcmp(str, "data") == 0)
+		return "mail | tail -n +2";
+	if(strcmp(str, "nadawca") == 0)
+		return "mail | tail -n +2 | sort -k3d -";
+
+	puts("Error!");
+	exit(1);
+}
 
 int main(int argc, char** argv) {
+	FILE* mail;
 	if(argc == 2) {
-		FILE* in = popen("mail", "w");		
+		mail = popen(commandSortMode(argv[1]), "w");
+		fputs("exit", mail);
+		pclose(mail);
+	} else if (argc == 4) {
+		char command[BUFFER_SIZE];
+		sprintf(command, "mail -s %s %s", argv[2], argv[1]);
+
+		mail = popen(command, "w");
+
+		fputs(argv[3], mail);
+		
+		pclose(mail);
 	}
 }
